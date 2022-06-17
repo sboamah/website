e838966f4761e1838b92c5b48fd50b1d25a995e4
Sylvia Boamah <60356081+sboamah@users.noreply.github.com>
2022-06-17 14:00:08 -0400

    hamburger menu

--- components/footer.js
+++ components/footer.js
@@ -1,131 +1,131 @@
 
-    footerTemplate = document.createElement('template');
+//     footerTemplate = document.createElement('template');
 
-    footerTemplate.innerHTML = `
-        <style>
-        footer{
-        margin: 0 auto;
-        text-align: center;
-        }
+//     footerTemplate.innerHTML = `
+//         <style>
+//         footer{
+//         margin: 0 auto;
+//         text-align: center;
+//         }
 
-        nav {
-        height: 80px;
-        margin: 0 auto;
-        position: relative;
-        width: 100%;
-        margin-top: 100px;
-        padding: 25px 0px;
-        text-align: center;
-        background: rgb(255, 210, 77);; 
-        }
+//         nav {
+//         height: 80px;
+//         margin: 0 auto;
+//         position: relative;
+//         width: 100%;
+//         margin-top: 100px;
+//         padding: 25px 0px;
+//         text-align: center;
+//         background: rgb(255, 210, 77); 
+//         }
 
-        ul li {
-        list-style: none;
-        display: inline;
-        text-transform: uppercase;
-        }
+//         ul li {
+//         list-style: none;
+//         display: inline;
+//         text-transform: uppercase;
+//         }
 
-        ul li a {
-            color: #000;
-            font-size: 12px;
-        }
+//         ul li a {
+//             color: #000;
+//             font-size: 12px;
+//         }
 
-        ul p{
-            list-style: none;
-            display: inline;
-            text-transform: uppercase;
-            font-size: 12px;
-            margin: 0 auto;
-            color: #000;
-        }
+//         ul p{
+//             list-style: none;
+//             display: inline;
+//             text-transform: uppercase;
+//             font-size: 12px;
+//             margin: 0 auto;
+//             color: #000;
+//         }
 
-        ul p a{
-            color: #000;
-        }
+//         ul p a{
+//             color: #000;
+//         }
 
-        img{
-            filter:grayscale(100%);
+//         img{
+//             filter:grayscale(100%);
           
 
-            width:20px;
-        }
-        .pic{
-          margin-bottom: -25px;
-     }
-        a {
-        font-weight: 100;
-        text-decoration: none;
-        margin-right: 5px;
-        }
+//             width:20px;
+//         }
+//         .pic{
+//           margin-bottom: -25px;
+//      }
+//         a {
+//         font-weight: 100;
+//         text-decoration: none;
+//         margin-right: 5px;
+//         }
 
 
-        a:hover {
-        padding-bottom: 3px;
-        transition: ease-in-out 1s;
-        box-shadow: inset 0 -2px 0 0 #fff;
-        }
+//         a:hover {
+//         padding-bottom: 3px;
+//         transition: ease-in-out 1s;
+//         box-shadow: inset 0 -2px 0 0 #fff;
+//         }
 
-        @media (max-width: 800px){
+//         @media (max-width: 800px){
 
-          nav {
-              font-size: 14px;
-          }
+//           nav {
+//               font-size: 14px;
+//           }
 
-        @media (max-width: 480px){
+//         @media (max-width: 480px){
           
-          .pic{
-             margin-left: -5px;
-             height:10px;
-             width: 100%;
-             margin-bottom: 0px;
-             display:block;
-        }
+//           .pic{
+//              margin-left: -5px;
+//              height:10px;
+//              width: 100%;
+//              margin-bottom: 0px;
+//              display:block;
+//         }
 
-        a{
-          margin-right: 2px;
-        }
-          nav {
-              font-size: 13px;
-              text-align: center;
-            }
+//         a{
+//           margin-right: 2px;
+//         }
+//           nav {
+//               font-size: 13px;
+//               text-align: center;
+//             }
 
-          ul  {
-              margin-left: -15px;
-          }
-            ul p{
-              font-size: 12px;
-              margin-left: -2px;
-          }
+//           ul  {
+//               margin-left: -15px;
+//           }
+//             ul p{
+//               font-size: 12px;
+//               margin-left: -2px;
+//           }
 
-          ul li {
-            margin-right: 15px;
-            }
-      }
-        </style>
-        <footer>
-        <nav>
-        <ul>
+//           ul li {
+//             margin-right: 15px;
+//             }
+//       }
+//         </style>
+//         <footer>
+//         <nav>
+//         <ul>
         
-        <li><a href="/">home</a></li>
-        <li><a href="work">work</a></li> 
-        <li><a href="https://drive.google.com/file/d/1nCj2IgyV3IqITM6QK9q1QeroiFJhU1P2/view?usp=sharing">resume</a></li>
-        <li><a href="contact">contact</a></li>
-        <li><a href="https://www.redbubble.com/people/jollofrican/shop?asc=u">shop</a></li>
-        <br><br>
+//         <li><a href="/">home</a></li>
+//         <li><a href="work">work</a></li> 
+//         <li><a href="https://drive.google.com/file/d/1nCj2IgyV3IqITM6QK9q1QeroiFJhU1P2/view?usp=sharing">resume</a></li>
+//         <li><a href="contact">contact</a></li>
+//         <li><a href="https://www.redbubble.com/people/jollofrican/shop?asc=u">shop</a></li>
+//         <br><br>
   
-        <p>Site by Sylvia Boamah.</p>
-        </ul>
-        </nav>
-        </footer>
-        `;
+//         <p>Site by Sylvia Boamah.</p>
+//         </ul>
+//         </nav>
+//         </footer>
+//         `;
 
-    class Footer extends HTMLElement {
-    constructor() {
-        super();
-        this.root = this.attachShadow({ mode: 'open' });
-        this.root.appendChild(footerTemplate.content);
-    }
+//     class Footer extends HTMLElement {
+//     constructor() {
+//         super();
+//         this.root = this.attachShadow({ mode: 'open' });
+//         this.root.appendChild(footerTemplate.content);
+//     }
 
-  }
+//   }
 
-customElements.define('my-footer', Footer);
+// customElements.define('my-footer', Footer);
--- components/navbar.js
+++ components/navbar.js
@@ -1,108 +1,135 @@
 
-    navTemplate = document.createElement('template');
+//     navTemplate = document.createElement('template');
 
-    navTemplate.innerHTML = `
-        <style>
-        nav {
-        height: 50px;
-        width: 100%;
-        position: relative;
-        margin: 0 auto;
-        padding: 5px 0px;
-        text-align: right;
-        vertical-align: middle;
-        background: rgb(255, 210, 77);;
-        }
+//     navTemplate.innerHTML = `
+//     <link rel="stylesheet" href="styles.css">
+//         <style>
+//         nav {
+//         height: 50px;
+//         width: 100%;
+//         position: relative;
+//         margin: 0 auto;
+//         padding: 5px 0px;
+//         text-align: right;
+//         vertical-align: middle;
+//         }
 
-        ul li {
-        font-size: 12px;
-        margin: 15px 5px;
-        list-style: none;
-        display: inline;
-        font-weight: 100;
-        text-transform: uppercase;
-        }
+//         ul li {
+//         font-size: 12px;
+//         margin: 15px 5px;
+//         list-style: none;
+//         display: block;
+//         font-weight: 100;
+//         text-transform: uppercase;
+//         }
 
-        .left{
-            float: left;
-            text-align: left;
-            margin-left: 20px;
-        }
+//         .left{
+//             float: left;
+//             text-align: left;
+//             margin-left: 20px;
+//         }
 
-        p {
-            font-size: 28px;
-            margin: 5px;
-            font-weight: 600;
-            display: inline;
-            text-transform: capitalize;
-        }
+//         p {
+//             font-size: 28px;
+//             margin: 5px;
+//             font-weight: 600;
+//             display: inline;
+//             text-transform: capitalize;
+//         }
 
-        a {
-        color: #000;
-        font-weight: 300;
-        text-decoration: none;
-        }
+//         ul {
+//             display: none;
+//         }
 
-        a:hover {
-        padding-bottom: 3px;
-        transition: ease-in-out 1s;
-        box-shadow: inset 0 -2px 0 0 #fff;
-        }
+//         a {
+//         color: #000;
+//         font-weight: 300;
+//         text-decoration: none;
+//         }
 
-        @media (max-width: 800px){
+//         a:hover {
+//         padding-bottom: 3px;
+//         transition: ease-in-out 1s;
+//         box-shadow: inset 0 -2px 0 0 #fff;
+//         }
 
-            nav {
-                font-size: 13px;
-            }
+//         @media (max-width: 800px){
 
-            p{
-                font-size: 20px;
-                margin: 10px;
-            }
+//             nav {
+//                 font-size: 13px;
+//             }
+
+//             p{
+//                 font-size: 20px;
+//                 margin: 10px;
+//             }
   
-        }
+//         }
 
-        @media (max-width: 497px){
-            p{
-                display:none;
-            }
+//         @media (max-width: 497px){
+//             p{
+//                 display:none;
+//             }
 
-            nav {
-                text-align: center;
-                font-size: 13px;
-            }
-            ul  {
-                margin-left: -35px;
-            }
-            a {
-                margin-right: 15px;
+//             nav {
+//                 text-align: center;
+//                 font-size: 13px;
+//             }
+//             ul  {
+//                 margin-left: -35px;
+//             }
+//             a {
+//                 margin-right: 15px;
                 
-            }
-        }
+//             }
+//         }
 
-        </style>
-        <header>
-        <nav>
-        <p class="left"><a href="/">Sylvia Boamah</a></p>
-        <ul>
-        
-        <li><a href="/">home</a></li>
-        <li><a href="work">work</a></li> 
-        <li><a href="https://drive.google.com/file/d/1nCj2IgyV3IqITM6QK9q1QeroiFJhU1P2/view?usp=sharing">resume</a></li>
-        <li><a href="contact">contact</a></li>
-        <li><a href="https://www.redbubble.com/people/jollofrican/shop?asc=u">shop</a></li>
-        </ul>
-        </nav>
-        </header>
-        `;
+//         </style>
+      
+//         <header>
+//         <nav>
+//         <p class="left"><a href="/">Sylvia Boamah</a></p>
+      
+//         <img id="hamburger" onclick="showLinks()" href="#" width="30px" src="images/Hamburger_icon.png">  
+//         <ul id ="menu">
+//             <li><a href="/">home</a></li>
+//             <li><a href="work">work</a></li> 
+//             <li><a href="https://drive.google.com/file/d/1nCj2IgyV3IqITM6QK9q1QeroiFJhU1P2/view?usp=sharing">resume</a></li>
+//             <li><a href="contact">contact</a></li>
+//             <li><a href="https://www.redbubble.com/people/jollofrican/shop?asc=u">shop</a></li>
+//         </ul>
+//         </nav>
+//         </header>
 
-    class Navbar extends HTMLElement {
-    constructor() {
-        super();
-        this.root = this.attachShadow({ mode: 'open' });
-        this.root.appendChild(navTemplate.content);
-    }
+//         <script>
+//         function showLinks(){
+//             let shown = false;
+//                 let menu =  document.getElementById('menu');
+//                 console.log(shown);
+//                 console.log(menu);
+//                     if (shown == false){
+//                         shown = true;
+//                         menu.style.display = block;
+//                     } else{
+//                         shown = false;
+//                             menu.style.display = none;
+//                 }
+//             }
+//         </script>
+//         `;
 
-  }
+//     class Navbar extends HTMLElement {
+//     constructor() {
+//         super();
+//         this.root = this.attachShadow({ mode: 'open' });
+//         this.root.appendChild(navTemplate.content);
+//     }
 
-customElements.define('my-navbar', Navbar);
+    
+//   }
+
+
+
+
+// customElements.define('my-navbar', Navbar);
+
